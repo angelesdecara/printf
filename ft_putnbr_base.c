c473b75afce7415a37a0b5de95b1d7dd5821301f
Angeles Rodriguez De Cara <angrodri@c2r11s5.42madrid.com>
2022-09-19 19:43:30 +0200

    sorted putnbr

--- ft_putnbr_base.c
+++ ft_putnbr_base.c
@@ -6,7 +6,7 @@
 /*   By: angrodri <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/09 17:28:38 by angrodri          #+#    #+#             */
-/*   Updated: 2022/09/12 18:00:36 by angrodri         ###   ########.fr       */
+/*   Updated: 2022/09/19 19:43:14 by angrodri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,37 +28,30 @@ void	ft_putnbr_base(int nbr, char *base)
 	long int	aux;
 
 	len = ft_strlen(base);
-	if (nbr == -2147483648)
-	{
-		aux = 2147483648 / (long int) len; 
-		write(1, "-", 1);
-		write(1, &aux, 1);
-		aux = 2147483648 / (long int) len;
-		nbr = aux;
-	}
-	if (nbr < 0)
+	aux = nbr;
+	if (aux < 0)
 	{
 		write(1, "-", 1);
-		nbr = (-1) * nbr;
+		aux = (-1) * aux;
 	}
-	if (nbr >= len)
+	if (aux < len)
+		write(1, &base[aux], 1);
+	else
 	{
-		ft_putnbr_base(nbr / len, base);
-		nbr = nbr % len;
+		ft_putnbr_base(aux / len, base);
+		ft_putnbr_base(aux % len, base);
 	}
-	if (nbr < len)
-		write(1, &base[nbr], 1);
 }
 
 int	main(void)
 {
-	char	base[] = "abcd";
+	char	base[] = "abcdefghij";
 	int		i;
 	int		len;
 
 	len = ft_strlen(base);
 	printf("base %d\n", len);
-	i = 0;
+	i = -10;
 	while (i < 10)
 	{
 		//if(i<4) write(1, &base[i], 1);
@@ -67,5 +60,6 @@ int	main(void)
 		i++;
 	}
 	ft_putnbr_base(2147483647, base);
+	printf("\n");
 	ft_putnbr_base(-2147483648, base);
 }
